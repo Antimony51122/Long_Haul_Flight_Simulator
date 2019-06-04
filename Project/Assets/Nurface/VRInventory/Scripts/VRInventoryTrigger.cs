dcd33982d3a42e99b7a3a82db9e782861f86c77f
Antimony51122 <zhuyaosheng51122@gmail.com>
2019-06-04 23:58:12 +0100

    inventory system in progress

--- Project/Assets/Nurface/VRInventory/Scripts/VRInventoryTrigger.cs
+++ Project/Assets/Nurface/VRInventory/Scripts/VRInventoryTrigger.cs
@@ -5,26 +5,22 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System;
 
-namespace MobileVRInventory
-{        
-    public class VRInventoryTrigger : MonoBehaviour
-    {
+namespace MobileVRInventory {
+    public class VRInventoryTrigger : MonoBehaviour {
         // Assigned by the menu item code
-        public VRInventory vrInventory;        
+        public VRInventory vrInventory;
 
-        void Start()
-        {
+        void Start() {
             if (!Application.isPlaying) return;
             if (vrInventory == null) return;
             CreateEvent(this.gameObject, EventTriggerType.PointerEnter, vrInventory.PointerEnterTrigger);
-            CreateEvent(this.gameObject, EventTriggerType.PointerExit, vrInventory.PointerExitTrigger);
+            CreateEvent(this.gameObject, EventTriggerType.PointerExit,  vrInventory.PointerExitTrigger);
         }
 
-        private void CreateEvent(GameObject o, EventTriggerType type, UnityAction action)
-        {
-            EventTrigger trigger = o.GetComponent<EventTrigger>();
+        private void CreateEvent(GameObject o, EventTriggerType type, UnityAction action) {
+            EventTrigger trigger         = o.GetComponent<EventTrigger>();
             if (trigger == null) trigger = o.AddComponent<EventTrigger>();
-            EventTrigger.Entry entry = new EventTrigger.Entry();
+            EventTrigger.Entry entry     = new EventTrigger.Entry();
             entry.eventID = type;
             entry.callback.AddListener((eventData) => { action.Invoke(); });
             trigger.triggers.Add(entry);
