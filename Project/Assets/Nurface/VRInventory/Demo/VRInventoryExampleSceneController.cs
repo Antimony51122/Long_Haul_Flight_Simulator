b112b94fcede101a3661d089615e607153146fbd
Antimony51122 <zhuyaosheng51122@gmail.com>
2019-06-04 20:07:10 +0100

    inventory system in progress

--- Project/Assets/Nurface/VRInventory/Demo/VRInventoryExampleSceneController.cs
+++ Project/Assets/Nurface/VRInventory/Demo/VRInventoryExampleSceneController.cs
@@ -4,30 +4,25 @@ using System.Collections;
 using System.Linq;
 using System;
 
-namespace MobileVRInventory
-{
-    public class VRInventoryExampleSceneController : MonoBehaviour
-    {
-        [Range(0, 100)]
-        public int health = 50;
+namespace MobileVRInventory {
+    public class VRInventoryExampleSceneController : MonoBehaviour {
+        [Range(0, 100)] public int health = 50;
 
-        [Range(0, 100)]
-        public int mana = 15;
+        [Range(0, 100)] public int mana = 15;
 
-        [Header("References")]
-        public VRInventory VRInventory;
+        [Header("References")] public VRInventory VRInventory;
 
-        public Image barsPanel;
+        public Image  barsPanel;
         public Slider healthSlider;
         public Slider manaSlider;
-        public Image coinsPanel;        
-        public Text coinsText;        
-        public Image messagePanel;
-        public Text messageText;        
+        public Image  coinsPanel;
+        public Text   coinsText;
+        public Image  messagePanel;
+        public Text   messageText;
 
         private int lastCoinValue = 0;
-        
-        void Start() {            
+
+        void Start() {
             // Listen to VR Inventory events
             VRInventory.onItemSelected.AddListener(ItemSelected);
             VRInventory.onItemPickedUp.AddListener(ItemPickedUp);
@@ -50,15 +45,21 @@ namespace MobileVRInventory
 
         void ItemSelected(InventoryItemStack stack) {
             switch (stack.item.name) {
-                case "Health Potion" : HandleHealthPotionUse(stack); break;
-                case "Mana Potion": HandleManaPotionUse(stack); break;
-            }            
+                case "Health Potion":
+                    HandleHealthPotionUse(stack);
+                    break;
+                case "Mana Potion":
+                    HandleManaPotionUse(stack);
+                    break;
+            }
         }
 
         void ItemPickedUp(VRInventory.InventoryItemPickupResult result) {
             if (result.result == MobileVRInventory.VRInventory.ePickUpResult.Success) {
                 switch (result.item.name) {
-                    case "Coin": UpdateCoinTextAnimated(); break;
+                    case "Coin":
+                        UpdateCoinTextAnimated();
+                        break;
                 }
             } else {
                 ShowMessage("You cannot carry anymore of those.");
@@ -90,12 +91,16 @@ namespace MobileVRInventory
 
             FadeInBars();
 
-            if(healthSlider != null) {
-                iTween.ValueTo(gameObject, iTween.Hash("from", healthSlider.value, "to", health, "time", time, "onupdate", "UpdateHealthBarAnimation", "easetype", "easeinsine"));                
+            if (healthSlider != null) {
+                iTween.ValueTo(gameObject,
+                    iTween.Hash("from", healthSlider.value, "to", health, "time", time, "onupdate",
+                        "UpdateHealthBarAnimation", "easetype", "easeinsine"));
             }
 
             if (manaSlider != null) {
-                iTween.ValueTo(gameObject, iTween.Hash("from", manaSlider.value, "to", mana, "time", time, "onupdate", "UpdateManaBarAnimation", "easetype", "easeinsine"));
+                iTween.ValueTo(gameObject,
+                    iTween.Hash("from", manaSlider.value, "to", mana, "time", time, "onupdate",
+                        "UpdateManaBarAnimation", "easetype", "easeinsine"));
             }
 
             FadeOutBars();
@@ -113,20 +118,22 @@ namespace MobileVRInventory
 
         void FadeOutBars() {
             iTween.StopByName(barsPanel.gameObject, "fadeOutBars");
-            iTween.ScaleTo(barsPanel.gameObject, iTween.Hash("scale", Vector3.zero, "time", 1f, "delay", 4f, "name", "fadeOutBars"));                  
-        }        
+            iTween.ScaleTo(barsPanel.gameObject,
+                iTween.Hash("scale", Vector3.zero, "time", 1f, "delay", 4f, "name", "fadeOutBars"));
+        }
 
         void FadeInBars() {
             iTween.StopByName(barsPanel.gameObject, "fadeInBars");
-            iTween.ScaleTo(barsPanel.gameObject, iTween.Hash("scale",Vector3.one, "time", 1f, "name", "fadeInBars"));            
-        }        
+            iTween.ScaleTo(barsPanel.gameObject, iTween.Hash("scale", Vector3.one, "time", 1f, "name", "fadeInBars"));
+        }
 
         void FadeOutCoins() {
             iTween.StopByName(coinsPanel.gameObject, "fadeOutCoins");
-            iTween.ScaleTo(coinsPanel.gameObject, iTween.Hash("scale", Vector3.zero, "time", 1f, "delay", 4f, "name", "fadeOutCoins"));
+            iTween.ScaleTo(coinsPanel.gameObject,
+                iTween.Hash("scale", Vector3.zero, "time", 1f, "delay", 4f, "name", "fadeOutCoins"));
         }
 
-        void FadeInCoins() {            
+        void FadeInCoins() {
             iTween.StopByName(coinsPanel.gameObject, "fadeInCoins");
             iTween.ScaleTo(coinsPanel.gameObject, iTween.Hash("scale", Vector3.one, "time", 1f, "name", "fadeInCoins"));
         }
@@ -135,44 +142,48 @@ namespace MobileVRInventory
             FadeInCoins();
 
             var valueBefore = Int32.Parse(coinsText.text.Replace("x", ""));
-            var valueAfter = VRInventory.GetItemQuantity("Coin");
+            var valueAfter  = VRInventory.GetItemQuantity("Coin");
 
             // Take longer depending on how many coins were added (but don't exceed 2 seconds)
             var time = Mathf.Min(0.1f * Math.Abs(valueAfter - valueBefore), 2f);
 
-            iTween.ValueTo(this.gameObject, iTween.Hash("from", valueBefore, "to", valueAfter, "time", time, "onupdate", "UpdateCoinText", "easetype", "easeinsine", "oncomplete", "FadeOutCoins"));
+            iTween.ValueTo(this.gameObject,
+                iTween.Hash("from", valueBefore, "to", valueAfter, "time", time, "onupdate", "UpdateCoinText",
+                    "easetype", "easeinsine", "oncomplete", "FadeOutCoins"));
         }
 
-        
+
         void UpdateCoinText(int newValue = -1) {
             if (lastCoinValue == newValue) return;
 
             if (newValue < 0) {
                 // if no value was provided, get it from the inventory
                 newValue = VRInventory.GetItemQuantity("Coin");
-            } 
+            }
 
             coinsText.text = String.Format("x{0}", newValue);
-            lastCoinValue = newValue;
+            lastCoinValue  = newValue;
         }
 
         public void ShowMessage(string message) {
             messageText.text = message;
-            FadeInMessage();            
+            FadeInMessage();
             FadeOutMessage();
         }
 
         private void FadeInMessage() {
             iTween.StopByName(messagePanel.gameObject, "fadeInMessage");
-            iTween.ScaleTo(messagePanel.gameObject, iTween.Hash("scale", Vector3.one, "time", 1f, "name", "fadeInMessage"));            
+            iTween.ScaleTo(messagePanel.gameObject,
+                iTween.Hash("scale", Vector3.one, "time", 1f, "name", "fadeInMessage"));
         }
 
         private void FadeOutMessage() {
             iTween.StopByName(messagePanel.gameObject, "fadeOutMessage");
-            iTween.ScaleTo(messagePanel.gameObject, iTween.Hash("scale", Vector3.zero, "time", 1f, "delay", 4f, "name", "fadeOutMessage"));
+            iTween.ScaleTo(messagePanel.gameObject,
+                iTween.Hash("scale", Vector3.zero, "time", 1f, "delay", 4f, "name", "fadeOutMessage"));
         }
 
-        public void Victory() {            
+        public void Victory() {
             ShowMessage("Victory! You can select the sword in your inventory again to put it away.");
         }
     }
