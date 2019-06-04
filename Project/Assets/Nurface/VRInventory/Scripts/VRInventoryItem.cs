21c7d205fd5799f7c74c9d38587fb5b7ed1e9295
Antimony51122 <zhuyaosheng51122@gmail.com>
2019-06-04 23:58:39 +0100

    inventory system in progress

--- Project/Assets/Nurface/VRInventory/Scripts/VRInventoryItem.cs
+++ Project/Assets/Nurface/VRInventory/Scripts/VRInventoryItem.cs
@@ -4,27 +4,23 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System;
 
-namespace MobileVRInventory
-{    
-    /// <summary>
-    /// This behaviour represents an item in the inventory window
-    /// </summary>
-    public class VRInventoryItem : MonoBehaviour
-    {
+namespace MobileVRInventory {
+    // This behaviour represents an item in the inventory window
+    public class VRInventoryItem : MonoBehaviour {
         public InventoryItemData itemData;
-        public int quantity = 1;
-        
+        public int               quantity = 1;
+
         public Image image;
-        public Text nameText;
-        public Text quantityText;
+        public Text  nameText;
+        public Text  quantityText;
 
         public void SetData(InventoryItemData data, int quantity = 1) {
             itemData = data;
 
-            if(image != null) image.sprite = data.image;
-            if(nameText != null) nameText.text = data.name;
-            
-            if (quantityText != null) {                
+            if (image    != null) image.sprite  = data.image;
+            if (nameText != null) nameText.text = data.name;
+
+            if (quantityText != null) {
                 quantityText.text = String.Format("x{0}", quantity);
                 // only show the quantity text if we have more than one of the item
                 quantityText.gameObject.SetActive(quantity > 1);
