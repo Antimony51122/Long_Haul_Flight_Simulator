acac7b91d3b282ef6d5fc4829612413833b02296
Antimony51122 <zhuyaosheng51122@gmail.com>
2019-06-04 23:57:33 +0100

    inventory system in progress

--- Project/Assets/Nurface/VRInventory/Scripts/Editor/InventoryItemEditor.cs
+++ Project/Assets/Nurface/VRInventory/Scripts/Editor/InventoryItemEditor.cs
@@ -4,23 +4,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace MobileVRInventory
-{
+namespace MobileVRInventory {
     [CustomEditor(typeof(InventoryItem))]
     [CanEditMultipleObjects()]
-    public class InventoryItemEditor : Editor
-    {        
-        private SerializedProperty itemDatabase;        
+    public class InventoryItemEditor : Editor {
+        private SerializedProperty itemDatabase;
         private SerializedProperty quantity;
 
         private InventoryItem itemTarget;
-        
-        void OnEnable() {            
-            itemDatabase = serializedObject.FindProperty("itemDatabase");            
-            quantity = serializedObject.FindProperty("quantity");
+
+        void OnEnable() {
+            itemDatabase = serializedObject.FindProperty("itemDatabase");
+            quantity     = serializedObject.FindProperty("quantity");
 
             itemTarget = target as InventoryItem;
-        }        
+        }
 
         public override void OnInspectorGUI() {
             serializedObject.Update();
@@ -29,15 +27,17 @@ namespace MobileVRInventory
 
             EditorGUI.BeginChangeCheck();
 
-            itemDatabase.objectReferenceValue = EditorGUILayout.ObjectField("Item Database", itemDatabase.objectReferenceValue, typeof(InventoryItemDatabase), false);
+            itemDatabase.objectReferenceValue = EditorGUILayout.ObjectField("Item Database",
+                itemDatabase.objectReferenceValue, typeof(InventoryItemDatabase), false);
 
             if (itemDatabase.objectReferenceValue == null) {
                 EditorGUILayout.HelpBox("Please select an Inventory Item Database", MessageType.Warning);
-            } else if(itemTarget.itemDatabase != null) {
-
-                var _itemDatabase = itemTarget.itemDatabase;
+            } else if (itemTarget.itemDatabase != null) {
+                var _itemDatabase  = itemTarget.itemDatabase;
                 var availableItems = _itemDatabase.items.Select(i => i.name).ToArray();
-                var selectedIndex = itemTarget.itemData != null ? availableItems.ToList().IndexOf(itemTarget.itemData.name) : 0;
+                var selectedIndex = itemTarget.itemData != null
+                    ? availableItems.ToList().IndexOf(itemTarget.itemData.name)
+                    : 0;
 
                 selectedIndex = EditorGUILayout.Popup("Item", selectedIndex, availableItems);
 
@@ -47,16 +47,16 @@ namespace MobileVRInventory
 
                 quantity.intValue = EditorGUILayout.IntField("Quantity", quantity.intValue);
             }
-            
-            if(EditorGUI.EndChangeCheck()) {
+
+            if (EditorGUI.EndChangeCheck()) {
                 serializedObject.ApplyModifiedProperties();
 
-                foreach (var t in targets)
-                {
+                foreach (var t in targets) {
                     (t as InventoryItem).itemData = itemData;
                 }
+
                 UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(itemTarget.gameObject.scene);
             }
-        }       
+        }
     }
 }
